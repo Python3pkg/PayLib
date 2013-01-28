305521e7d51cfd5db1dba72891b2ca88c08fc391
Matt Rutherford <matrutherford@gmail.com>
2013-01-28 00:48:34 +0000

    bugfix

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='paylib',
-      version='0.2',
+      version='0.2.1',
       description='Simple PayPal Express Checkout API abstraction for python',
       url='https://github.com/rutherford/PayLib',
       author='Matt Rutherford',
       author_email='rutherford@clientsideweb.net',
       license='MIT',
-      packages=['paylib'],
+      packages=find_packages(),
       zip_safe=False)
